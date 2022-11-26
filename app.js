dce61f1feae30fe0844218aeac281a443b30fcbd
ravi kumar dhawan <ravidhawan8899@gmail.com>
2022-11-26 23:29:29 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+"use strict";
+exports.__esModule = true;
+var App = /** @class */ (function () {
+    function App() {
+    }
+    App.prototype.test = function () {
+        console.log("demo fun");
+    };
+    return App;
+}());
+var a1 = new App();
+a1.test();
---
+++ app.ts
@@ -0,0 +1,8 @@
+export {}
+class App {
+    test(){
+        console.log("demo fun");
+    }
+}
+let a1 = new App();
+a1.test();
