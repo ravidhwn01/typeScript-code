a5745d994987eba77af9ce2837921aba6c765b6e
ravi kumar dhawan <ravidhawan8899@gmail.com>
2022-11-30 18:44:13 +0530

    promise in ts

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,37 @@
+interface Ipost{
+    title:string;
+    id:number;
+    deccription:string;
+}
 
-const fetchData = async (path:string) => {
+interface Iuser{
+    id:number;
+    name:string;
+    age:string;
+}
+const fetchData1 = async (path:string):Promise<Ipost[]> => {
+    const response = await fetch(`http://example.com${path}`)
+    return response.json();
+}
+const fetchData2 = async (path:string):Promise<Iuser[]> => {
     const response = await fetch(`http://example.com${path}`)
     return response.json();
 }
 // anonymous func
 (
    async () => {
-   const post = await fetchData('/post')
-   post[0]
+   const posts = await fetchData1('/post')
+   posts[0].deccription;
+   posts[0].id;
+   posts[0].title;
    }
-)()
+)();
+// anonymous func
+(
+   async () => {
+   const user = await fetchData2('/user')
+   user[0].age;
+   user[0].id;
+   user[0].name;
+   }
+)();
