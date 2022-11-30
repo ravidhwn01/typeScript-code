a91e6746db155716a1e558353b5c4b7686fb6555
ravi kumar dhawan <ravidhawan8899@gmail.com>
2022-12-01 01:26:52 +0530

    duck typing concept

--- src/duckType.ts
+++ src/duckType.ts
@@ -1,6 +1,7 @@
 interface Icreadential {
   username: string;
   password: string;
+  age?:number;
 }
 
 function logIn1(creadentials: Icreadential): boolean {
@@ -8,10 +9,13 @@ function logIn1(creadentials: Icreadential): boolean {
   return true;
 }
 
-// creating object  hamne isko type nhi diya 😲 that is called duck typing
-const data = {
+// creating object  hamne isko type nhi diya 😲 that is called duck typing / structural typing
+const data : Icreadential = {
   username: "ravi",
   password: "12345678",
   age:34
 };
 logIn1(data);
+
+
+// agar 2 object ki shape  same ho to   typescript ke hisaab se vo 2no same ype ke hai
